2031fa9b7dd0d46914150617ae9641a35155b41f
ZAKARIA <elguazzarzakaria10@gmail.com>
2025-05-19 11:23:08 +0100

    Some modifications

---
+++ src/app/login.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {jwtDecode} from 'jwt-decode';
+import {Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  isAutenticated : boolean = false;
+  roles : any;
+  username : any;
+  accessToken !: any;
+
+  constructor(private http:HttpClient,private router:Router) {
+  }
+  public Login(username:string,password:string){
+    let options =
+      {
+        headers:new HttpHeaders().set("Content-Type", "application/x-www-form-urlencoded")
+      }
+    let params = new HttpParams().set("username",username).set("password",password);
+    return this.http.post("http/login",params,options);
+  }
+
+  loadProfile(data:any) {
+    this.isAutenticated=true;
+    this.accessToken= data["access_token"];
+    let decodedJwt:any= jwtDecode(this.accessToken);
+    this.username = decodedJwt.sub;
+    this.roles = decodedJwt.scope;
+    window.localStorage.setItem("accessToken",this.accessToken);
+  }
+
+  logout() {
+    this.isAutenticated=false;
+    this.username=undefined;
+    this.roles=undefined;
+    this.accessToken=undefined;
+    window.localStorage.removeItem("accessToken");
+    this.router.navigateByUrl("/login")
+  }
+
+  loadJwtTokenFromLocalStorage() {
+    let token = window.localStorage.getItem("accessToken");
+    this.accessToken = token;
+    this.loadProfile({"access_token":token});
+    this.router.navigateByUrl("/admin/customers");
+
+  }
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,53 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {LoginService} from '../services/Login/login.service';
+import {NgIf} from '@angular/common';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit{
+  private LoginFormGroup !: FormGroup;
+  private messageError !: Object;
+  public constructor(private loginService : LoginService , private fb:FormBuilder,private router:Router) {
+  }
+
+  ngOnInit(): void {
+    this.LoginFormGroup = this.fb.group({
+      username: this.fb.control("",[Validators.required]),
+      password: this.fb.control("",[Validators.required])
+    });
+
+  }
+
+
+  handleLoginSubmit() {
+    let username:string = this.LoginFormGroup?.value.username;
+    let password:string = this.LoginFormGroup?.value.password;
+    this.loginService.Login(username,password).subscribe(
+      {
+        next:data=>{
+          this.loginService.loadProfile(data);
+          this.router.navigateByUrl("/admin");
+        },
+        error:(err)=>{
+          this.messageError=err;
+        }
+      }
+    );
+  }
+  getLoginFormGroup(){
+    return this.LoginFormGroup;
+  }
+  getMessageError(){
+    return this.messageError;
+  }
+}
